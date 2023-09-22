87f5e13114b70044f9dbb722862a49dfcfa2a077
Beef <mark@benewith.com>
2023-09-22 15:07:49 +0100

    rewrite portfolio script

--- templates/partials/portfolio-grid.php
+++ templates/partials/portfolio-grid.php
@@ -13,21 +13,18 @@ if($posts) { ?>
           <?php $thumb = get_field('portfolio_thumb'); ?>
           <?php $thumb_id = is_array($thumb) ? $thumb['id'] : $thumb; ?>
           <div class="w-1/2 md:w-1/3 masonry-item p-0.5 relative">
-            <?php echo wp_get_attachment_image($thumb_id, 'large'); ?>
-  
-            <?php $gallery_images = get_field('portfolio_images'); ?>
-            <?php if($gallery_images) { ?>
-              
-              <div class="masonry-item-title inset-0 absolute flex justify-center items-center text-black bg-white no-underline m-0.5 bg-opacity-80 opacity-0 transition-opacity dx-caps p-4">
-                <?php foreach($gallery_images as $key => $gallery_image) { ?>
-                    <a href="<?php echo $gallery_image['url']; ?>" data-fancybox="<?php echo $post->post_name; ?>" data-caption="<?php echo $gallery_image['caption']; ?>" class="absolute inset-0 <?php echo($key > 0) ? 'hidden' : null; ?>">
-                      <?php echo wp_get_attachment_image($gallery_image['id'], 'large', null, array('class' => 'hidden')); ?> 
-                    </a>
-                <?php } ?>
-                <div class="p-2 text-center">
-                  <?php echo get_the_title(); ?>
-                </div>
+            <a href="<?php echo $thumb['url']; ?>" data-fancybox="<?php echo $post->post_name; ?>" data-caption="<?php echo $thumb['caption']; ?>" class="relative block">
+              <?php echo wp_get_attachment_image($thumb_id, 'large'); ?>
+              <div class="absolute inset-0 flex justify-center items-center text-black bg-white no-underline bg-opacity-80 opacity-0 hover:opacity-100 transition-opacity dx-caps p-4">
+                <?php echo get_the_title(); ?>
               </div>
+            </a>
+          </div>
+          
+          <div class="hidden">
+            <?php $gallery_images = get_field('portfolio_images'); ?>
+            <?php foreach($gallery_images as $key => $gallery_image) { ?>
+              <?php echo wp_get_attachment_image($gallery_image['id'], 'large', false, array('data-fancybox' => $post->post_name, 'data-caption' => $gallery_image['caption'])); ?> 
             <?php } ?>
           </div>
         <?php } ?>
