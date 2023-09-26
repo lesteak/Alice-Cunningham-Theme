5f5e83556be8b82319cbb450d0e0f77b617f173a
Beef <mark@benewith.com>
2023-09-26 15:19:04 +0100

    jquery requirement by square

--- footer.php
+++ footer.php
@@ -35,6 +35,10 @@ if (get_theme_mod('footer_sticky') === 'enabled') : ?>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+  integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+  crossorigin="anonymous"></script>
 
 <?php wp_footer(); ?>
 
